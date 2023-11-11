17b9a7971c098cb57ef0e7f30d787c46ebff0292
Graciána Adamcová <adamcova.grace@gmail.com>
2023-11-11 14:58:48 +0100

    component SelectedSeat - zobrazení sedadla SVG

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import { JourneyPicker } from '../../components/JourneyPicker';
 import { JourneyDetail } from '../../components/JourneyDetail';
+import { SelectedSeat } from '../../components/SelectedSeat';
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null);
+  const [number, setNumber] = useState(null);
 
   const handleJourneyChange = (journey) => {
     setJourney(journey.stops);
-    console.log(journey.stops);
+    setNumber(journey.autoSeat);
   };
 
   return (
@@ -15,6 +17,7 @@ export const HomePage = () => {
       <JourneyPicker onJourneyChange={handleJourneyChange} />
       {/* <p>Nalezeno spojení s id {journey}</p> */}
       {journey === null ? null : <JourneyDetail journey={journey} />}
+      {journey === null ? null : <SelectedSeat number={number} />}
     </main>
   );
 };
